981db21516b4c4b91683cf0aeddfb1222a7ff730
Leegumin <littlegbear@naver.com>
2022-05-29 16:32:48 +0900

    2022-05-29

---
+++ showpping_mall_back/src/test/java/com/example/showpping_mall_back/ShowppingMallBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.showpping_mall_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowppingMallBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
