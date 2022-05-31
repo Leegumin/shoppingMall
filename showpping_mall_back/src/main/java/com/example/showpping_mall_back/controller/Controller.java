27deff80c4f8ac1cd8c1cc9ca19e662f7bdc318b
Leegumin <littlegbear@naver.com>
2022-06-01 04:46:48 +0900

    2022-06-01

--- showpping_mall_back/src/main/java/com/example/showpping_mall_back/controller/Controller.java
+++ showpping_mall_back/src/main/java/com/example/showpping_mall_back/controller/Controller.java
@@ -2,11 +2,14 @@ package com.example.showpping_mall_back.controller;
 
 import com.example.showpping_mall_back.model.Member;
 import com.example.showpping_mall_back.service.MemberServiceImp;
+import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,14 +30,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 // *@RequestMapping("/api") : http://localhost:8000/ -> http://localhost:8000/api로 기본주소 변경됨
 @RequestMapping("/api")
+@NoArgsConstructor
 public class Controller {
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     MemberServiceImp memberService;
+
 //    암호화 보류
-//    @Autowired
-//    BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     // 회원가입, insert
     // *ResponseEntity : frontend 요청 시 객체를 결과로 전달
@@ -44,7 +49,7 @@ public class Controller {
             @RequestBody
             Member member) {
         logger.info("registerMember - member : {} ", member);
-
+        passwordEncoder.encode(member);
         // *성공 여부 판단
         boolean bSuccess = memberService.save(member);
         logger.info("bSuccess : {} ", bSuccess);
